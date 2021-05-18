dc28fb1fd1af84e4418a479001a6e8ba77f00430
kushstriker <83347729+kushstriker@users.noreply.github.com>
2021-05-18 19:15:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = " ";
+    recognition.start();
+}
+
+recognition.onresult = function run (event) {
+ 
+console.log(event);    
+}
